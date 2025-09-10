1880f3261c9820bbd1cab91da2cedf145d8d352c
princessmanifest <jasmine.adamsestl@gmail.com>
2025-09-09 19:05:34 -0700

    This assignment is complete

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,22 +24,30 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+// console.log("Welcome to the bootcamp / Syntax Error
+// console.log("Welcome to the bootcamp");
 
-// What’s Wrong?
+// What’s Wrong? - Missing Closing and colon
 
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+// let numbers = [2, 4, "eight"]; // Runtime error
+// for (let i = 0; i < numbers.length; i++) {
+//   let doubled = numbers[i] * 2;
+//   console.log(doubled);
+// }
+
+
+let numbers = [2, 4,];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
-// What’s Wrong?
+// What’s Wrong? - the srting "eight" is not a number and therefore when behanving As an operand in the Multiplication problem in returns NaN
 
 
 
@@ -47,16 +55,27 @@ for (let i = 0; i < numbers.length; i++) {
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
+// function isPrime(num) { // Logical error 
+//   if (num < 2) return false;
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return true;  // Supposed to indicate num is NOT prime
+//     }
+//   }
+//   return false; // Supposed to indicate num IS prime
+// }
+
+
 function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
-// What’s Wrong?
+// What’s Wrong? - The bool asisgnment logic was just exactly backwards
